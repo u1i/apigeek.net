de4dab5311217ddb7a69ae24b217312b3b1fb2d5
Uli Hitzel <uli.hitzel@gmail.com>
2021-12-28 15:02:35 +0800

    Tue Dec 28 15:02:35 +08 2021

--- art/thenewworld/mini/create_montage.sh
+++ art/thenewworld/mini/create_montage.sh
@@ -1,1 +1,1 @@
-montage *jpg -geometry 100x100+3+3 out.png
+montage *jpg -tile 8x4 -geometry 100x100+3+3 out.png
--- art/ukiyoe/mini/create_montage.sh
+++ art/ukiyoe/mini/create_montage.sh
@@ -1,1 +1,1 @@
-montage *jpg -geometry 100x100+3+3 out.png
+montage *jpg -tile 8x6 -geometry 100x100+3+3 out.png
