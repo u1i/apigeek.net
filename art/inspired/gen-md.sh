606aefb20c4f011b29132c1382c99be7d8855ae6
Uli Hitzel <uli.hitzel@gmail.com>
2022-01-04 14:39:10 +0800

    Tue Jan  4 14:39:10 +08 2022

--- art/inspired/gen-md.sh
+++ art/inspired/gen-md.sh
@@ -3,6 +3,18 @@
 #| --- | --- |
 #EOF
 
+def gen_capt()
+{
+
+	file=$1
+	caption=$2
+
+	convert -background white  -fill black -gravity South -font helvetica -pointsize 18 -size 320x   caption:"$caption" -bordercolor grey -border 10 captions/$2
+
+bevelborder -s 8 -m inner -c 25 -b red -a 15 caption1.jpg cx.jpg
+
+}
+
 cp gallery.templ README.md
 
 cat list.txt | xargs -n2 > /tmp/gen.txt
@@ -16,6 +28,9 @@ do
 
 	capt1=$(./get-caption-from-file.sh $pic1)
 	capt2=$(./get-caption-from-file.sh $pic2)
+
+	gen_capt($pic1 $capt1)
+	gen_capt($pic2 $capt2)
 
 	echo "| [![](framed/${pic1})]($pic1) | [![](framed/${pic2})]($pic2) |" >> README.md
 	echo "| $capt1 | $capt2 |" >> README.md
