b903c886c5a8af92127ee532aa03f17dfed9baab
Uli Hitzel <uli.hitzel@gmail.com>
2022-01-04 20:54:45 +0800

    Tue Jan  4 20:54:45 +08 2022

--- art/ukiyoe/mini/create_montage.sh
+++ art/ukiyoe/mini/create_montage.sh
@@ -1,3 +1,8 @@
 # montage *jpg -tile 8x2 -geometry 100x100+3+3 out.png
 
 montage $(cat list.txt) -tile 4x2 -geometry 200x200+3+3 out.png
+
+montage $(cat list2.txt) -tile 4x2 -geometry 200x200+9+9 out2-tmp.png
+convert -rotate 357 out2-tmp.png out2-tmp2.png
+convert out2-tmp2.png -crop 824x412+10+10 -geometry 824x412 +repage out2.png
+
