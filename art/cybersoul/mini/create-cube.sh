eaa266a94db4f44bcebf194b8476adb9199b82fd
Uli Hitzel <uli.hitzel@gmail.com>
2022-01-05 10:43:41 +0800

    Wed Jan  5 10:43:41 +08 2022

---
+++ art/cybersoul/mini/create-cube.sh
@@ -0,0 +1,2 @@
+3Dbox pan=45 tilt=-45 pef=1 filter=point alien-planet.jpg chinatown.jpg the-face.jpg c1.jpg
+3Dbox pan=45 tilt=-45 pef=1 filter=point night-out.jpg office-buildings.jpg street-scene.jpg c2.jpg
